655aba8748f4feaf03f5e5ec10ae8783a5a62505
Benny Schneider <benny.schneider@moneymeets.com>
2017-11-21 13:47:27 +0100

    Use implicit return types when possible

--- multi-functions/src/main/kotlin/io/multifunctions/MultiForEach.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiForEach.kt
@@ -2,17 +2,17 @@ package io.multifunctions
 
 import io.multifunctions.models.*
 
-inline fun <A, B> Iterable<Pair<A?, B?>>.forEach(action: (A?, B?) -> Unit): Unit =
+inline fun <A, B> Iterable<Pair<A?, B?>>.forEach(action: (A?, B?) -> Unit) =
         this.forEach { elem -> action(elem.first, elem.second) }
 
-inline fun <A, B, C> Iterable<Triple<A?, B?, C?>>.forEach(action: (A?, B?, C?) -> Unit): Unit =
+inline fun <A, B, C> Iterable<Triple<A?, B?, C?>>.forEach(action: (A?, B?, C?) -> Unit) =
         this.forEach { elem -> action(elem.first, elem.second, elem.third) }
 
-inline fun <A, B, C, D> Iterable<Quad<A?, B?, C?, D?>>.forEach(action: (A?, B?, C?, D?) -> Unit): Unit =
+inline fun <A, B, C, D> Iterable<Quad<A?, B?, C?, D?>>.forEach(action: (A?, B?, C?, D?) -> Unit) =
         this.forEach { elem -> action(elem.first, elem.second, elem.third, elem.fourth) }
 
-inline fun <A, B, C, D, E> Iterable<Penta<A?, B?, C?, D?, E?>>.forEach(action: (A?, B?, C?, D?, E?) -> Unit): Unit =
+inline fun <A, B, C, D, E> Iterable<Penta<A?, B?, C?, D?, E?>>.forEach(action: (A?, B?, C?, D?, E?) -> Unit) =
         this.forEach { elem -> action(elem.first, elem.second, elem.third, elem.fourth, elem.fifth) }
 
-inline fun <A, B, C, D, E, F> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.forEach(action: (A?, B?, C?, D?, E?, F?) -> Unit): Unit =
+inline fun <A, B, C, D, E, F> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.forEach(action: (A?, B?, C?, D?, E?, F?) -> Unit) =
         this.forEach { elem -> action(elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth) }
--- multi-functions/src/main/kotlin/io/multifunctions/MultiForEachIndexed.kt
+++ multi-functions/src/main/kotlin/io/multifunctions/MultiForEachIndexed.kt
@@ -2,17 +2,17 @@ package io.multifunctions
 
 import io.multifunctions.models.*
 
-inline fun <A, B> Iterable<Pair<A?, B?>>.forEachIndexed(action: (Int, A?, B?) -> Unit): Unit =
+inline fun <A, B> Iterable<Pair<A?, B?>>.forEachIndexed(action: (Int, A?, B?) -> Unit) =
         this.forEachIndexed { index, elem -> action(index, elem.first, elem.second) }
 
-inline fun <A, B, C> Iterable<Triple<A?, B?, C?>>.forEachIndexed(action: (Int, A?, B?, C?) -> Unit): Unit =
+inline fun <A, B, C> Iterable<Triple<A?, B?, C?>>.forEachIndexed(action: (Int, A?, B?, C?) -> Unit) =
         this.forEachIndexed { index, elem -> action(index, elem.first, elem.second, elem.third) }
 
-inline fun <A, B, C, D> Iterable<Quad<A?, B?, C?, D?>>.forEachIndexed(action: (Int, A?, B?, C?, D?) -> Unit): Unit =
+inline fun <A, B, C, D> Iterable<Quad<A?, B?, C?, D?>>.forEachIndexed(action: (Int, A?, B?, C?, D?) -> Unit) =
         this.forEachIndexed { index, elem -> action(index, elem.first, elem.second, elem.third, elem.fourth) }
 
-inline fun <A, B, C, D, E> Iterable<Penta<A?, B?, C?, D?, E?>>.forEachIndexed(action: (Int, A?, B?, C?, D?, E?) -> Unit): Unit =
+inline fun <A, B, C, D, E> Iterable<Penta<A?, B?, C?, D?, E?>>.forEachIndexed(action: (Int, A?, B?, C?, D?, E?) -> Unit) =
         this.forEachIndexed { index, elem -> action(index, elem.first, elem.second, elem.third, elem.fourth, elem.fifth) }
 
-inline fun <A, B, C, D, E, F> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.forEachIndexed(action: (Int, A?, B?, C?, D?, E?, F?) -> Unit): Unit =
+inline fun <A, B, C, D, E, F> Iterable<Hexa<A?, B?, C?, D?, E?, F?>>.forEachIndexed(action: (Int, A?, B?, C?, D?, E?, F?) -> Unit) =
         this.forEachIndexed { index, elem -> action(index, elem.first, elem.second, elem.third, elem.fourth, elem.fifth, elem.sixth) }
